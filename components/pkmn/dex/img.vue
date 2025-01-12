562253f1a10eaf69e43189eba7f92a3fde3188a4
mathematicalninja <github@mathematics.ninja>
2025-01-12 21:20:46 +0000

    update for grid placement

--- components/pkmn/dex/img.vue
+++ components/pkmn/dex/img.vue
@@ -1,49 +1,21 @@
 <script setup lang="tsx">
 import { usePkmnGetImgPath } from "~/composables/pkmn/getImgPath";
-import HeightWidthFromParent from "~/utils/calculate/HeightWidthFromParent";
-import HeightWidthRatio from "~/utils/calculate/HeightWidthRatio";
 
 const props = defineProps({
   dexNum: { type: Number, required: true },
   genNum: { type: Number, required: true },
-  parent_width: { type: Number, required: false, default: 100 },
-  parent_height: { type: Number, required: false, default: 100 },
 });
-
-function getImageHeightWidth(path: string): [number, number] {
-  return [20, 20];
-}
-
-const [imgHeight, imgWidth] = getImageHeightWidth(
-  usePkmnGetImgPath(props.genNum, props.dexNum)
-);
-
-const [calcHeight, calcWidth] = HeightWidthFromParent(
-  HeightWidthRatio(imgHeight, imgWidth),
-  props.parent_height,
-  props.parent_width
-);
 </script>
 
 <template>
-  <!-- <div>Component: pkmn/dex/img</div> -->
-  <div class="flex pokePicContainer">
+  <div
+    class="flex border-white border-2 h-52 w-52 align-center justify-center fit-inside hover:scale-125"
+  >
     <NuxtImg
-      :width="calcHeight"
-      :height="calcWidth"
-      class="justify-self-center pokePicImage"
+      @load="alert(9)"
       :src="usePkmnGetImgPath(props.genNum, props.dexNum)"
-      fit="inside"
     />
   </div>
 </template>
 
-<style scoped>
-.pokePicContainer {
-  border-width: 2px;
-}
-
-.pokePicImage {
-  object-fit: contain;
-}
-</style>
+<style scoped></style>
