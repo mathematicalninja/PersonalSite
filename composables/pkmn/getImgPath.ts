af90841b6c044b47297344c3ddbd4c3c965b8616
mathematicalninja <github@mathematics.ninja>
2025-01-20 20:16:08 +0800

    cleaned up code

--- composables/pkmn/getImgPath.ts
+++ composables/pkmn/getImgPath.ts
@@ -1,16 +1,11 @@
 // import * as path from "node:path";
 
 export const usePkmnGetImgPath = (gen: number, dexNum: number) => {
-  const pathArray: string[] = [
+  return [
     "img",
     "pkmn",
     "png",
     "gen" + gen.toString(),
     dexNum.toString() + ".png",
-  ];
-  const join = (paths: string[]) => {
-    return paths.join("/");
-  };
-
-  return join(pathArray);
+  ].join("/");
 };
