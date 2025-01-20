ea04f43c7ce48c083a730e547b86089226e6cedb
mathematicalninja <github@mathematics.ninja>
2025-01-21 07:47:47 +0800

    Added dex number caps

--- constants/pkmn.ts
+++ constants/pkmn.ts
@@ -4,3 +4,21 @@ export const FullArtFolderPath = [
     "png",
     "fullArt",
   ].join("/");
+
+
+
+
+export const genCaps = {
+    1:151,
+    2:251,
+    3:386,
+    4:493,
+    5:649,
+    6:721,
+    7:809,
+    8:905,
+    9:1025
+}
+
+
+export const maxSensaibleGenNum:number= 7
