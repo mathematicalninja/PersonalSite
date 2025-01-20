92d50900013837957c08af6f4ee8a838d0dd2672
mathematicalninja <github@mathematics.ninja>
2025-01-21 07:48:32 +0800

    refactored json path

--- composables/pkmn/getImageJson.ts
+++ composables/pkmn/getImageJson.ts
@@ -1,14 +1,16 @@
-export const usePkmnGetImageJson = (genNum:Number, dexNum:Number) => {
-  
-  return ref()
+// import { readFile } from "fs/promises";
+import data from "~/public/img/pkmn/png/fullArt/img_data.json"
+import { FullArtFolderPath } from "~/constants/pkmn";
+type PkmnImageJson = {
+  height:number,
+  width:number,
+  name:string
 }
 
-export const usePkmnGetImgPath = (gen: number, dexNum: number) => {
-  return [
-    "img",
-    "pkmn",
-    "png",
-    "gen" + gen.toString(),
+export const usePkmnGetFullArtJson = (genNum:Number, dexNum:Number) => {
+
+  const p= [
+    FullArtFolderPath,
     dexNum.toString() + ".json",
   ].join("/");
 };
