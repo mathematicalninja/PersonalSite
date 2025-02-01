3919b87215c45389c2f085d2871f52cc648e22ef
mathematicalninja <github@mathematics.ninja>
2025-02-01 14:56:29 +0800

    Added auto-imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ["@nuxt/ui", "@nuxt/image"],
   compatibilityDate: "2024-12-23",
+  imports:{
+    dirs:[
+      'composables/**',
+      'utils/**',
+      'typss/**'
+    ]
+  },
 });
