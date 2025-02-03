51013f72a91f997cbccab46352687701387e6335
mathematicalninja <github@mathematics.ninja>
2025-02-03 18:30:16 +0800

    clickCard uses a callback function

--- components/nlogn/clickCard.vue
+++ components/nlogn/clickCard.vue
@@ -1,8 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { UContainer } from '#components';
+
+const props = defineProps({
+  h:{
+    type:Number,
+    required:false,
+    default:100
+  },
+  w:{
+    type:Number,
+    required:false,
+    default:100
+  },
+  onClick: {
+    type:Function,
+    required:true,
+  }
+});
+</script>
 
 <template>
-  <div>
-    Component: nlogn/clickCard
+  <div
+    @click="() => onClick()"
+  >
+    <UContainer  
+      style="padding: 0%;"
+    >
+
+      <slot/>
+    </UContainer>
   </div>
 </template>
 
