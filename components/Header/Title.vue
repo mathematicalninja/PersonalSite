6c3dc36579f19fe198a6714eb815e4b027c9cd01
mathematicalninja <github@mathematics.ninja>
2025-02-03 18:33:14 +0800

    Tweaked Header

--- components/Header/Title.vue
+++ components/Header/Title.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 // import { UCard } from "#build/components";
+const ui = /* ui */ {
+  background: 'bg-white dark:bg-slate-900'
+}
+
+const cardui = /* ui */ "text-5xl"
+
 </script>
 
 <template>
-  <div class="text-5xl">Component: Header/Title</div>
-  <UCard class="text-5xl"> Alex Duncan </UCard>
+  <!-- <div class="text-5xl">Component: Header/Title</div> -->
+  <UCard 
+  :ui="{base:'p-0 m-0 text-5xl'}"
+  > 
+    Alex Duncan
+    <HeaderTagline />
+  </UCard>
 </template>
 
 <style scoped></style>
