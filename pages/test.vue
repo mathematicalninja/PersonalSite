4a126c2fa521cf9a1efffc7aaa3326962393d2c6
mathematicalninja <github@mathematics.ninja>
2025-02-03 18:36:42 +0800

    Testing Randomised Arrays

--- pages/test.vue
+++ pages/test.vue
@@ -1,78 +1,130 @@
 <script setup lang="ts">
+import { PkmnDexImg } from '#components';
+import { jobs } from 'googleapis/build/src/apis/jobs';
+import ClickCard from '~/components/nlogn/clickCard.vue';
 import { lazyData } from '~/composables/pkmn/getImageJson';
-import HeightWidthFromParent from '~/utils/calculate/HeightWidthFromParent';
-import HeightWidthRatio from '~/utils/calculate/HeightWidthRatio';
+import {range} from '~/utils/manipulation/range';
+import HeightWidthRatio from "~/utils/calculate/HeightWidthRatio";
+import randomiseArray from '~/utils/array/randomise';
+
 
 type pokeNumber = { dexNum: number; genNum: number };
 
-const dexNum = ref(151);
-const range = 8;
-const piles: pokeNumber[] = [{ dexNum: 1, genNum: 3 }];
+const genCap = 9;
+const pileCap = 2
 
 
-const props = {
-  dexNum: dexNum,
-  genNum: 6,
-  parent_height: 140,
-  parent_width: 140,
+
+const pileA = ref<pokeNumber[]>([
+  {dexNum: 1, genNum: 1},
+  {dexNum: 2, genNum: 1},
+  {dexNum: 3, genNum: 1},  
+])
+const pileB = ref<pokeNumber[]>([
+  {dexNum: 4, genNum: 1},
+  {dexNum: 5, genNum: 1},
+  {dexNum: 6, genNum: 1},  
+])
+
+const pileC = ref<pokeNumber[]>([
+  {dexNum: 7, genNum: 1},
+  {dexNum: 8, genNum: 1},
+  {dexNum: 9, genNum: 1},  
+])
+
+const piles= computed(()=>{return[pileA, pileB, pileC]});
+
+function choosePile(pile:any[],refPile:any):void {
+  pile.reverse()
+  const  h = pile.pop()
+  pile.reverse()
+  refPile.push(h)
+  console.log(pile)
+}
+const ar = range(genCap)
+
+const dexNums = ref<pokeNumber[]>([]);
+const dNums = ref<NestedArray<pokeNumber>>([]);
+// ar.map((x) => dNums.value.push({dexNum: x, genNum: 1}))
+
+const v:NestedArray<pokeNumber> = []
+
+const w = computed(()=>{return randomiseArray(ar)})
+// BUG: this randomises the list twice, once on load, and twice on refresh
+
+for (let x of w.value) {
+  v.push({dexNum: x, genNum: 1})
 }
 
-// const props = defineProps({
-//   dexNum: { type: Number, required: true },
-//   genNum: { type: Number, required: true },
-
-//   parent_height: { type: Number, required: false, default: 140 },
-//   parent_width: { type: Number, required: false, default: 140 },
-// });
+const o = recursiveDistribute(v, pileCap)
 
 
-// const [h,w] = lazyData(props.dexNum.value);
-// const [h,w] = [computed(() => lazyData(dexNum.value)[0]),computed(() => lazyData(dexNum.value)[1])]
-const h = computed(() => lazyData(dexNum.value)[0])
-const w = computed(() => lazyData(dexNum.value)[1])
+// const k = randomiseArray(dNums.value)
+// const v=recursiveDistribute(dNums.value, pileCap)
+const t = ref([])
 
-const sizes = computed(() => lazyData(dexNum.value))
 
-// const [h,w] = sizes.value
+/*
+Idea: create a generator function that takes in a NestedArray<T>, and at each "comparison step" it exposes "something" to wait for user input to be used as the comparison step.
 
-// const r = HeightWidthRatio(h,w)
-// const [height,width] = HeightWidthFromParent(r,props.parent_height,props.parent_width)
+The function should run recursivly (obviously), and companre the elements to T:
+  - if the elements are a collection of Array<T>, then each of those elements can be thought of as a sorted list. 
+  - otherwise the contents is a NestedArray<Array<T>>, ==> recusion.
 
-// Creating a writable computed ref:
-
-const count = ref(dexNum)
-const plusOne = computed({
-  get: () => count.value + 1,
-  set: (val) => {
-    count.value = val
-  }
-})
+When we have "dug down" to the second from bottom elements of the NestedArray<T> we should get an array of Array<T>
+We then merge the elements of these arrays, starting with the first elements of each Array<T> being exposed to the user for comparison, and the "winner" being added to the return object as the first element.
+Iterating until all element Array<T> are empty, then returning the sorted list (type Array<T>).
+  - Note this will remove one layer of NestedArray<Array<T>> returning a NestedArray<T>.
+  - if the recursive function is given a NestedArray<T> as input, it will return it unchanged (assuming it is an already sorted list.)
+*/
 
 
 </script>
 
 <template>
-
-  <div>
-    h: {{ h }}
-    w: {{ w }}
-  </div>
-<UButton @click="plusOne=151"> Mew </UButton>
-<UButton @click="plusOne=300"> skitty </UButton>
-<UButton @click="plusOne=14"> Kakhuna </UButton>
-
-
+<p> dexNums: {{ dexNums }}</p>
+<p> ar: {{ ar }}</p>
+<p> w: {{ w }}</p>
+<p> v: {{ v }}</p>
+<p> o: {{ o }}</p>
+<p> t: {{ t }}</p>
   <UContainer 
-    class="flex border-green-500 border-2 align-center justify-center   bg-black "
+    class="flex border-green-500 border-2 align-center justify-center h-96  bg-black "
     style="padding: 0%;"
     width="500"
   >
-    <PkmnDexImg
-      :dex-num="dexNum"
-      :gen-num="1"
-      :parent_height="300"
-      :parent_width="300"
-    />
+  <div
+    class=""
+  >
+    <div
+    class="flex justify-center"
+    >
+      <ClickCard
+        v-for="pile in piles"
+        :onClick="() => choosePile(pile.value, dexNums) "
+      >
+        <PkmnDexImg 
+        v-if="pile.value.length > 0"
+          :dex-num="pile.value[0].dexNum" 
+          :gen-num="1" 
+          :parent_height="140"
+        :parent_width="140"
+        />
+      </ClickCard>
+    </div>
+    
+    <div
+    class="flex justify-center">
+      
+      <PkmnDexImg  
+      v-for="pkmn in dexNums"
+          :dex-num="pkmn.dexNum" 
+          :gen-num="pkmn.genNum" 
+          :parent_height="140"
+        :parent_width="140"
+        />
+    </div>
+  </div>
   </UContainer>
 </template>
 
