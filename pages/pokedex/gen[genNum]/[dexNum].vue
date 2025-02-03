c6e85822eb34975f80e13079e9d4feb6fa60fd14
mathematicalninja <github@mathematics.ninja>
2025-02-03 18:28:58 +0800

    Added placeholder pages for pokedex entries

---
+++ pages/pokedex/gen[genNum]/[dexNum].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    poekdex
+  </div>
+  <div>Generation: {{ genNum }}</div>
+  <div># {{ dexNum }}</div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+
+// When accessing /posts/1, route.params.id will be 1
+const genNum = route.params.genNum
+const dexNum = route.params.dexNum
+
+</script>
+
+<style>
+
+</style>
