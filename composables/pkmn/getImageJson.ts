4029ea1e188ba207ffb0b897ecd277a52c3e5592
mathematicalninja <github@mathematics.ninja>
2025-02-08 17:10:57 +0800

    Added function to "simple" retrieve name and dexnum

--- composables/pkmn/getImageJson.ts
+++ composables/pkmn/getImageJson.ts
@@ -1,8 +1,9 @@
 // import { readFile } from "fs/promises";
 import data from '~/public/img/pkmn/png/fullArt/img_data.json'
 import { FullArtFolderPath } from '~/constants/pkmn'
+import type { PkmnImageJson, pokeCardInfo } from '~/types/pkmn'
 
-export const usePkmnGetFullArtJson = (genNum: Number, dexNum: Number) => {
+export const usePkmnGetFullArtJsonPath = (dexNum: Number) => {
     const p = [FullArtFolderPath, dexNum.toString() + '.json'].join('/')
 }
 
@@ -15,4 +16,23 @@ export function lazyData(dexNum: number): [number, number] {
         }
     }
     return [140, 140]
+}
+
+
+export function getPkmnImageJson(dexNum: number, genNum:number): pokeCardInfo {
+    
+    function stripedNum(element:PkmnImageJson) {return Number(element.name.split('.')[0])}
+    data.sort((a,b)=>{
+        return stripedNum(a) > stripedNum(b) ? 1 : -1
+    })
+    const dataElement =  data[dexNum-1] // Note that the dexNum is 1-indexed, but the array is 0-indexed
+    console.log(dataElement)
+    
+    return {
+        name: dataElement.name,
+        num: {
+            dexNum: dexNum,
+            genNum: genNum
+        }
+    }
 }
