0370aff0ddac347e812e09f38bd24b8062c1181f
mathematicalninja <github@mathematics.ninja>
2025-02-08 17:11:53 +0800

    Added pokemon card inspired Card.vue

---
+++ components/pkmn/dex/card.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="pokeCard">
+        <ClickCard
+            :onClick="() => props.onClick()"
+            :w="props.w"
+            :h="props.h"
+        >
+        <div class="divCenterer">
+            <div
+                class="pkmnImg"
+            >
+                <PkmnDexImg
+                    :dex-num="props.pokeNumber.num.dexNum"
+                    :gen-num="props.pokeNumber.num.genNum"
+                    :parent_height="props.w"
+                    :parent_width="props.w"
+                />
+            </div>
+            <div style="padDiv" />
+            <p class="bottomAligned">#{{ props.pokeNumber.num.dexNum }} {{ props.pokeNumber.name }}</p>
+        </div>
+         
+        </ClickCard>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import type { _borderColor } from '#tailwind-config/theme'
+    import type { pokeCardInfo } from '~/types/pkmn'
+
+    const props = defineProps({
+        h: {
+            type: Number,
+            required: false,
+            default: 190,
+        },
+        w: {
+            type: Number,
+            required: false,
+            default: 140,
+        },
+        onClick: {
+            type: Function,
+            required: true,
+        },
+        pokeNumber: {
+            type: Object as () => pokeCardInfo,
+            required: true,
+        },
+    })
+    const borderWidth = 7
+    const textHeight = 30
+    const imgHeight = props.w
+    const padSize = props.h-textHeight-2*borderWidth
+</script>
+
+<style>
+.pokeCard {
+    border: v-bind(borderWidth +"px") solid yellow;
+    border-radius: 10px;
+    width: v-bind(w + 'px');
+    height: v-bind(h + 'px');
+    display: flex;
+    flex-direction: column;
+}
+
+.divCenterer {
+    /* border: 3px solid red; */
+    display: flex;
+    flex-direction: column;
+    /* flex: 1; */
+    /* justify-content: space-between; */
+}
+.bottomAligned {
+    /* border: 2px solid green; */
+    /* margin-top: auto; */
+    /* align-self: flex-end; */
+    flex-basis: v-bind(textHeight + "px");
+    padding-left: 5px; /*TODO: make this a variable */
+}
+.pkmnImg {
+    /* border: 4px solid purple; */
+    /* display: flex; */
+    /* justify-content: center; */
+    /* border-2 border-purple-500 */
+    flex-basis: v-bind(padSize+"px");
+}
+.padDiv{
+    /* border: 4px solid blue; */
+    flex-grow: 100;
+    flex-basis: v-bing(padSize+"px");
+}
+</style>
