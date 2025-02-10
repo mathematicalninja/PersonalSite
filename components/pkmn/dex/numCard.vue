1ab5f75f7e0ebe81aa50fe5cdd87c6be65a9078f
mathematicalninja <github@mathematics.ninja>
2025-02-10 23:23:27 +0800

    add in a "full Art" version of pkmnDexCard that only relies on the dex num Fixes #78

---
+++ components/pkmn/dex/numCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <PkmnDexCard
+        :h="props.h"
+        :w="props.w"
+        :onClick="props.onClick"
+        :pokeNumber="pokeNumber"
+    />
+</template>
+
+<script lang="ts" setup>
+    import { PkmnDexCard } from '#components';
+    import type { pokeCardInfo } from '~/types/pkmn';
+    import { getPkmnImageJson } from '~/composables/pkmn/getImageJson';
+
+    const props = defineProps({
+
+    h: {
+        type: Number,
+        required: false,
+        default: 190,
+    },
+    w: {
+        type: Number,
+        required: false,
+        default: 140,
+    },
+    onClick: {
+        type: Function,
+        required: true,
+    },
+    dexNum: {
+        type: Number,
+        required: true,
+    },
+    })
+
+    const pokeNumber:pokeCardInfo = getPkmnImageJson(props.dexNum, 1)
+
+</script>
+
+<style>
+
+</style>
