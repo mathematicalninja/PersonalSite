bd150ce635cfb6936515d4b3f033dd3b04b55c77
mathematicalninja <github@mathematics.ninja>
2025-02-13 18:21:57 +0800

    Basic config

--- pages/nlogn/demo.vue
+++ pages/nlogn/demo.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    
+    {{ unsortedInt }}
   </div>
+  <div
+  class="border-2 border-white w-16 h-16
+   text-5xl flex justify-center items-center"
+
+  >
+
+    {{ unsortedInt[0] }}
+  </div>
+  
 </template>
 
 <script lang="ts" setup>
+import type { NestedArray } from '~/types/NestedArray';
+import randomiseArray from '~/utils/array/randomise';
+
+
+const unsortedInt:NestedArray<Number> = randomiseArray(range(16))
 
 </script>
 
 <style>
+
 
 </style>
