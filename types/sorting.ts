636a70a96e266d937e0bf39054009c16a1b147f5
mathematicalninja <github@mathematics.ninja>
2025-02-16 16:30:54 +0800

    Added typeGuards for Atoms and AtomicArrays

--- types/sorting.ts
+++ types/sorting.ts
@@ -4,17 +4,28 @@ export type SortAtom<T> = {
     state: 'atom'
     data: T
 }
+export function isAtom<T>(arr: RecursiveSortArray<T>): arr is SortAtom<T> {
+    return arr.state === 'atom'
+}
 
-export type SortArray<T> = {
+// An array of tagged atoms.
+export type AtomicArray<T> = {
     state: SortState
-    data: Array<T>
+    data: Array<SortAtom<T>>
+}
+
+export function isAtomicArray<T>(
+    arr: RecursiveSortArray<T>,
+): arr is AtomicArray<T> {
+    if (isAtom(arr)) {
+        return false
+    }
+    return arr.data.every((e) => isAtom(e))
 }
 
 export type RecursiveSortArray<T> =
     //   Just an element
     | SortAtom<T>
-    // Final array level
-    | SortArray<T>
     | {
           //Full recursive array
           state: SortState
