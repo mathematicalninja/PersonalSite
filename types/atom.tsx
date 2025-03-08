4a16f1bc3b6e1ba898ec531c52e1aef668686627
mathematicalninja <github@mathematics.ninja>
2025-03-08 12:41:35 +0800

    Removed defunct comment

--- types/atom.tsx
+++ types/atom.tsx
@@ -1,12 +1,5 @@
 import type { JSX } from 'vue/jsx-runtime'
 
-// export interface AtomProps<AtomData> {
-//     data: AtomData
-//     render: (data: AtomData) => JSX.Element
-// }
-
-// export interface AtomData extends Record<string, any> {}
-
 export interface Atom<Data> {
     data: Data
     render: () => JSX.Element
