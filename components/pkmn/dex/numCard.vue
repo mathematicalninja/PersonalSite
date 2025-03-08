d3737840aca966b42ff04c2c35cfd362119cc7ad
mathematicalninja <github@mathematics.ninja>
2025-03-08 13:14:50 +0800

    Refactor numCard.vue to provide a default onClick function and improve prop structure; update demo.vue for better rendering of unsorted integers and recursive sorting.

--- components/pkmn/dex/numCard.vue
+++ components/pkmn/dex/numCard.vue
@@ -8,37 +8,37 @@
 </template>
 
 <script lang="ts" setup>
-    import { PkmnDexCard } from '#components';
-    import type { pokeCardInfo } from '~/types/pkmn';
-    import { getPkmnImageJson } from '~/composables/pkmn/getImageJson';
+    import { PkmnDexCard } from '#components'
+    import type { pokeCardInfo } from '~/types/pkmn'
+    import { getPkmnImageJson } from '~/composables/pkmn/getImageJson'
 
     const props = defineProps({
-
-    h: {
-        type: Number,
-        required: false,
-        default: 190,
-    },
-    w: {
-        type: Number,
-        required: false,
-        default: 140,
-    },
-    onClick: {
-        type: Function,
-        required: true,
-    },
-    dexNum: {
-        type: Number,
-        required: true,
-    },
+        h: {
+            type: Number,
+            required: false,
+            default: 190,
+        },
+        w: {
+            type: Number,
+            required: false,
+            default: 140,
+        },
+        onClick: {
+            type: Function,
+            required: false,
+            default: () => {
+                return
+            },
+        },
+        dexNum: {
+            type: Number,
+            required: true,
+        },
     })
 
-    const pokeNumber = computed(() => getPkmnImageJson(props.dexNum, 1));
+    const pokeNumber = computed(() => getPkmnImageJson(props.dexNum, 1))
     // TODO: #92 Refactor pkmnDexNumCard to retyrn "blank" pokeball card if no dexNum is provided
     // TODO: #93 Allow pkmnDexNumCard (and upstream) to accept no function
 </script>
 
-<style>
-
-</style>
+<style></style>
--- pages/nlogn/demo.vue
+++ pages/nlogn/demo.vue
@@ -1,43 +1,63 @@
 <template>
-  <div>
-    {{ unsortedInt }}
-  </div>
-<div class="text-4xl">
-Last clicked element: {{ clickedElement }}
-</div>
-<AlignmentNmGrid :n="4" :m="4">
-  <template #grid-item="{ index }">
-    <div
-    class="border-2 border-white text-5xl w-16 h-16 "
-      v-if="index < unsortedInt.length" 
-      :key="index" 
-    >
-    <NlognClickCard
-    :onClick="()=>{clickedElement= unsortedInt[index] as number}">
-
-      <AlignmentCenterDiv>
-        
-        {{ unsortedInt[index] }}
-      </AlignmentCenterDiv>
-    </NlognClickCard>
+    <div>
+        {{ unsortedInt }}
     </div>
-  </template>
-</AlignmentNmGrid>
-  
+
+    <AlignmentNmGrid
+        :n="4"
+        :m="4"
+    >
+        <template #grid-item="{ index }">
+            <div
+                class="border-2 border-white text-5xl w-16 h-16"
+                v-if="index < unsortedInt.length"
+                :key="index"
+            >
+                <AlignmentCenterDiv>
+                    {{ unsortedInt[index] }}
+                </AlignmentCenterDiv>
+            </div>
+        </template>
+    </AlignmentNmGrid>
+    {{ recursiveInts }}
 </template>
 
 <script lang="ts" setup>
-import type { NestedArray } from '~/types/NestedArray';
-import randomiseArray from '~/utils/array/randomise';
+    import type { NestedArray } from '~/types/NestedArray'
+    import type { pokeNumber } from '~/types/pkmn'
+    import type { RecursiveSortArray } from '~/types/sorting'
+    import randomiseArray from '~/utils/array/randomise'
+    import DexNum from '../pokedex/gen[genNum]/[dexNum].vue'
 
-const clickedElement = ref<number|string>("none")
+    const intCount = 16
+    const unsortedInt: NestedArray<Number> = randomiseArray(range(intCount))
 
-const unsortedInt:NestedArray<Number> = randomiseArray(range(16))
+    const recursiveInts = recursiveTagAndDistribute(range(intCount), 4)
 
+    /**
+     * TODO: need to be able to pass a "renderable" into the recursiveTagAndDistribute function, or use its output to generate renderables.
+     *
+     *
+     *  Need a "dig down" function that gets down to the list of Atomics.
+     *  This is what "B" does
+     *
+     *  Use these atoms to generate renderables.
+     *       ==> have a component that the atom can be passed to as a prop.
+     *  Use these renderables to generate a grid.
+     *  This os what "A" does
+     * */
 
+    // TODO: Here we need to make it so
+    // A1. Atomic Arrays are checked for
+    // A2. Atomic Arrays can be rendered
+    // A3. Atomic Arrays can be sorted in place
+
+    // B1. Sorted Arrays (as elements) can be checked for.
+    // B2. Sorted arrays can have their top element (which should be atomic) used as its representative element.
+    // B3. these elements can be rendered
+    // B4. these elements can be used in the sorting algorithm to create a unified pile.
+
+    // C1. Sorted Piles can be checked for, and passed
 </script>
 
-<style>
-
-
-</style>
+<style></style>
