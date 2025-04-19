547df4e062b5da909874aa8fba8e941af923bc39
mathematicalninja <github@mathematics.ninja>
2025-04-19 12:48:40 +0800

    Assorted Types added

--- types/sorting.ts
+++ types/sorting.ts
@@ -1,3 +1,5 @@
+import type { Atom } from './atom'
+
 /**
  * partial is for when the array was "abandoned" in the middle of sorting
  *
@@ -28,6 +30,11 @@ export type SortArray<T> = {
     data: Array<RecursiveSortArray<T>>
 }
 
+/**
+ * Recursive type, ground state: SortAtom, Layers of SortArray
+ */
+export type RecursiveSortArray<T> = SortAtom<T> | SortArray<T>
+
 export function isSortArray<T>(
     arr: RecursiveSortArray<T>,
 ): arr is SortArray<T> {
@@ -39,12 +46,89 @@ export type SortArrayAtoms<T> = {
     data: AtomicArray<T>
 }
 
+export type RenderableSortArrayOrAtom<T> =
+    | SortAtom<Atom<T>>
+    | SortArray<Atom<T>>
+
 /**
  * An **array** of tagged atoms.
  */
 export type AtomicArray<T> = Array<SortAtom<T>>
 
-/**
- * Recursive type, ground state: SortAtom, Layers of SortArray
- */
-export type RecursiveSortArray<T> = SortAtom<T> | SortArray<T>
+export type nonRecursiveSortArray<T> = {
+    state: SortArrayState
+    data: Array<SortAtom<Atom<T>> | SortArrayAtoms<Atom<T>>>
+}
+
+export type AtomOrSortedArray<T> =
+    | SortAtom<T>
+    | {
+          state: 'sorted'
+          data: Array<SortAtom<T>>
+      }
+
+export function isSorted<T>(ar: RecursiveSortArray<T>): ar is SortedArray<T> {
+    return ar.state === 'sorted'
+}
+
+export function isArrayOfAtomOrSortArray<T>(
+    ar: Array<RecursiveSortArray<T>>,
+): ar is Array<AtomOrSortedArray<T>> {
+    for (let element of ar) {
+        if (!(isAtom(element) || isSortArray(element))) {
+            return false
+        }
+    }
+
+    return true
+}
+
+export function areAllPilesAtoms<T>(
+    piles: Array<RenderableSortArrayOrAtom<T>>,
+): piles is Array<SortAtom<Atom<T>>> {
+    for (let i = 0; i < piles.length; i++) {
+        const p = piles[i]
+        if (!p) {
+            return false // error
+        }
+        if (!(p.state == 'atom')) {
+            return false
+        }
+    }
+    return true
+}
+
+export function noAtomsInPiles<T>(
+    piles: Array<RenderableSortArrayOrAtom<T>>,
+): piles is Array<SortArray<Atom<T>>> {
+    for (let i = 0; i < piles.length; i++) {
+        const p = piles[i]
+        if (!p) {
+            return false // error
+        }
+        if (p.state == 'atom') {
+            return false
+        }
+    }
+    return true
+}
+
+export type SortedArray<T> = { state: 'sorted'; data: Array<SortAtom<T>> }
+
+export function AreAllSorted<T>(
+    ar: Array<SortArray<T> | SortAtom<T>>,
+): ar is Array<SortedArray<T>> {
+    // ): ar is Array<SortArray<T>> {
+    return ar.every((p) => {
+        return !isAtom(p) && isSorted(p)
+    })
+}
+
+export function areAllSortedOrAtoms<T>(ar: Array<SortArray<T> | SortAtom<T>>) {
+    for (let element of ar) {
+        if (!(element.state == 'atom' || element.state == 'sorted')) {
+            return false
+        }
+    }
+    return true
+}
