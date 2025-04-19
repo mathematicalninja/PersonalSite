fc76bf7d691e46c0562e7057a4c3c3a2bcee3c5c
mathematicalninja <github@mathematics.ninja>
2025-04-19 12:21:54 +0800

    Added render components to use in Templates

---
+++ components/Render/Default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ props.dataRenderFunction() }}
+    </div>
+</template>
+
+<script lang="tsx" setup>
+    import type { JSX } from 'vue/jsx-runtime'
+    const props = defineProps<{
+        dataRenderFunction: () => JSX.Element
+    }>()
+
+    // // ~~~ Psuedo code for renderFunction ~~~
+    // function renderFunction() {
+    //     return <div>{{ defaultData }}</div>
+    // }
+</script>
+
+<style></style>
