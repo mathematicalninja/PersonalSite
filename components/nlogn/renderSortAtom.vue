0e080b209130732838f1b3f78981f0385b36ddf7
mathematicalninja <github@mathematics.ninja>
2025-04-19 12:40:40 +0800

    Better render component for Atoms

---
+++ components/nlogn/renderSortAtom.vue
@@ -0,0 +1,32 @@
+<template> { at } </template>
+
+<script lang="tsx" setup>
+    import type { Atom } from '~/types/atom'
+    import type { SortAtom } from '~/types/sorting'
+    import type { JSX } from 'vue/jsx-runtime'
+
+    const props = defineProps({
+        atom: {
+            type: Object as PropType<SortAtom<Atom<any>>>,
+            required: true,
+        },
+        atomRenderFunction: {
+            type: Function as PropType<(atom: SortAtom<any>) => JSX.Element>,
+            required: false,
+            default: (atom: SortAtom<any>) => {
+                return <div>{atom.data}</div>
+            },
+        },
+    })
+    const at = props.atom.data.render()
+    // const RenderSortAtom = defineComponent({
+    //     props: {},
+    //     setup(props) {
+    //         return () => {
+    //             return props.atom.data.render()
+    //         }
+    //     },
+    // })
+</script>
+
+<style></style>
