5a3e36941f3a97436b4cf7aba44e1e8a180850b2
mathematicalninja <github@mathematics.ninja>
2025-04-19 12:23:02 +0800

    Tweaked nmGrid to move away from kabab-case nonsense

--- components/Alignment/nmGrid.vue
+++ components/Alignment/nmGrid.vue
@@ -1,52 +1,70 @@
 <template>
-  <div v-for="i in mRange" :key="i" class="grid-row">
-    <div v-for="h in nRange" :key="h" class="grid-item">
-        <slot name="grid-item" :index="(i - 1) * props.n + (h - 1)"/> 
-        <!-- TODO: #97 refactor "grid-item" in nmGrid as gridIndex -->
+    <div class="nmGrid">
+        <div
+            v-for="i in mRange"
+            :key="i"
+            class="gridRow"
+        >
+            <div
+                v-for="h in nRange"
+                :key="h"
+                class="gridItem"
+            >
+                <slot
+                    name="gridItem"
+                    :index="(i - 1) * props.n + (h - 1)"
+                />
+                <!-- TODO: #97 refactor "gridItem" in nmGrid as gridIndex -->
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
-  /**
-   * nmGrid.vue
-   * 
-   * This component generates a grid layout for an array of html/vue elements based on the provided `m` and `n` props.
-   * - `n` represents the number of rows.
-   * - `m` represents the number of columns.
-   * 
-   * Each grid item is rendered using a slot, allowing for flexible content insertion.
-   * The `index` of each grid item is calculated and passed to the slot as a prop.
-   */
-  const props = defineProps({
-    m:{
-      type: Number,
-      required: true
-    },
-    n:{
-      type: Number,
-      required: true
-    }
-  })
+    /**
+     * nmGrid.vue
+     *
+     * This component generates a grid layout for an array of html/vue elements based on the provided `m` and `n` props.
+     * - `n` represents the number of rows.
+     * - `m` represents the number of columns.
+     *
+     * Each grid item is rendered using a slot, allowing for flexible content insertion.
+     * The `index` of each grid item is calculated and passed to the slot as a prop.
+     */
+    const props = defineProps({
+        m: {
+            type: Number,
+            required: true,
+        },
+        n: {
+            type: Number,
+            required: true,
+        },
+    })
 
-  const mRange = range(props.m)
-  const nRange = range(props.n)
-
+    const mRange = range(props.m)
+    const nRange = range(props.n)
 </script>
 
 <style scoped>
-.grid-row {
-  display: flex;
-}
-.grid-item {
-  display: flex;
-}
+    .gridRow {
+        display: flex;
+    }
+    .gridItem {
+        display: flex;
+    }
+    .nmGrid {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
 <!-- Example implementation: -->
 <!--
 <div>
   <nm-grid :m="4" :n="4">
-    <template #grid-item="{ index }">
+    <template #gridItem="{ index }">
       <div v-if="index < testNums.length" :key="index" class="border-2 border-white text-5xl w-16 h-16 ">
         <AlignmentCenterDiv>
           {{ testNums[index] }}
