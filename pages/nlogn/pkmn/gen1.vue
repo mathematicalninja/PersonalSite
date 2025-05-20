d7716cfe2bf8d9e32b8fab86b4fe9966f268bd33
mathematicalninja <github@mathematics.ninja>
2025-05-20 11:49:27 +0800

    updated to new type signiture Added overflow protection centered bottom Images on current Dex num

--- pages/nlogn/pkmn/gen1.vue
+++ pages/nlogn/pkmn/gen1.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     import Hover from '~/components/Alignment/Hover.vue'
-    import { usePkmnGetFullArtPath } from '~/composables/pkmn/getImgPath'
-    const dexNum = ref(45)
+    import { usePkmnGetFullArtPath_National } from '~/composables/pkmn/getImgPath'
+    const numInDex = ref(45)
     const picWidth = 140
     const picHeight = 140
     const genMin = 1
@@ -12,22 +12,35 @@
 
 <template>
     <div>Page: nlogn/pkmn/gen1</div>
-    <div>path = {{ usePkmnGetFullArtPath(1, 1) }}</div>
+    <div>path = {{ usePkmnGetFullArtPath_National(1) }}</div>
     <div class="flex-row">
         <div class="">
             <UButton
                 class=""
-                @click="dexNum > 1 ? (dexNum -= 1) : (dexNum = dexNum)"
+                @click="
+                    numInDex > genMin ? (numInDex -= 1) : (numInDex = numInDex)
+                "
             >
                 -
             </UButton>
-            <UButton @click="dexNum += 1"> + </UButton>
+            <UButton
+                class=""
+                @click="
+                    numInDex < genMax ? (numInDex += 1) : (numInDex = numInDex)
+                "
+            >
+                +
+            </UButton>
         </div>
 
         <div class="flex justify-center p-2 border-2 border-red-700">
             <div v-for="i in [0, 1, 2]">
                 <div
-                    v-for="h in [dexNum + i, dexNum + i + 3, dexNum + i + 6]"
+                    v-for="h in [
+                        numInDex + i,
+                        numInDex + i + 3,
+                        numInDex + i + 6,
+                    ]"
                     class="flex justify-center"
                     :style="{
                         width: picWidth + 'px',
@@ -37,8 +50,7 @@
                     }"
                 >
                     <PkmnDexImg
-                        :dex-num="h"
-                        :gen-num="1"
+                        :pokeDexNum="{ genNum: 1, numInDex: h }"
                         :parent_height="picHeight"
                         :parent_width="picWidth"
                     />
@@ -47,23 +59,16 @@
         </div>
 
         <div class="flex">
+            <PkmnDexImg :pokeDexNum="{ genNum: 1, numInDex: numInDex - 1 }" />
             <PkmnDexImg
-                :dex-num="dexNum"
-                :gen-num="1"
-            />
-            <PkmnDexImg
-                :dex-num="dexNum + 1"
-                :gen-num="1"
+                :pokeDexNum="{ genNum: 1, numInDex: numInDex }"
                 :parent_height="picHeight"
                 :parent_width="picWidth"
             />
-            <PkmnDexImg
-                :dex-num="dexNum + 2"
-                :gen-num="1"
-            />
+            <PkmnDexImg :pokeDexNum="{ genNum: 1, numInDex: numInDex + 1 }" />
         </div>
     </div>
-    <span>Number: {{ dexNum }}</span>
+    <span>Number: {{ numInDex }}</span>
 </template>
 
 <style scoped></style>
