78fdc18ec429111017a11de4349c3c818bc83bc8
mathematicalninja <github@mathematics.ninja>
2025-05-20 11:24:45 +0800

    Refactored to new type signitures, and added 0==>pokeball.png

--- composables/pkmn/getImgPath.ts
+++ composables/pkmn/getImgPath.ts
@@ -1,9 +1,21 @@
 import { FullArtFolderPath } from '~/constants/pkmn'
 
-export const usePkmnGetFullArtPath = (genNum: number, dexNum: number) => {
-    if (dexNum == 0) {
+import type { NationalNum, PokeDexNumber } from '~/types/component/pkmn'
+export const usePkmnGetFullArtPath = (pkDexNum: PokeDexNumber) => {
+    // TODO: fix for oddities in pkmn nums
+    if (pkDexNum.numInDex == 0) {
         return [FullArtFolderPath, 'pokeball.png'].join('/')
     }
-    const p = [FullArtFolderPath, dexNum.toString() + '.png'].join('/')
+    const p = [FullArtFolderPath, pkDexNum.numInDex.toString() + '.png'].join(
+        '/',
+    )
+    return p
+}
+
+export const usePkmnGetFullArtPath_National = (NationalDexNum: NationalNum) => {
+    if (NationalDexNum == 0) {
+        return [FullArtFolderPath, 'pokeball.png'].join('/')
+    }
+    const p = [FullArtFolderPath, NationalDexNum.toString() + '.png'].join('/')
     return p
 }
