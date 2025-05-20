b8dad37c2225e9da901f920d439b92d148d2d77a
mathematicalninja <github@mathematics.ninja>
2025-05-20 12:06:11 +0800

    hid overflow

--- pages/nlogn/pkmn/gen1.vue
+++ pages/nlogn/pkmn/gen1.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     import Hover from '~/components/Alignment/Hover.vue'
     import { usePkmnGetFullArtPath_National } from '~/composables/pkmn/getImgPath'
-    const numInDex = ref(45)
+    const numInDex = ref(1)
     const picWidth = 140
     const picHeight = 140
     const genMin = 1
@@ -11,10 +11,10 @@
 </script>
 
 <template>
-    <div>Page: nlogn/pkmn/gen1</div>
-    <div>path = {{ usePkmnGetFullArtPath_National(1) }}</div>
+    <!-- <div>Page: nlogn/pkmn/gen1</div>
+    <div>path = {{ usePkmnGetFullArtPath_National(1) }}</div> -->
     <div class="flex-row">
-        <div class="">
+        <div class="p-2 flex justify-center">
             <UButton
                 class=""
                 @click="
@@ -23,6 +23,8 @@
             >
                 -
             </UButton>
+
+            <span class="p-2">Number: {{ numInDex }}</span>
             <UButton
                 class=""
                 @click="
@@ -37,9 +39,9 @@
             <div v-for="i in [0, 1, 2]">
                 <div
                     v-for="h in [
-                        numInDex + i,
-                        numInDex + i + 3,
-                        numInDex + i + 6,
+                        numInDex + i - 4,
+                        numInDex + i - 1,
+                        numInDex + i + 2,
                     ]"
                     class="flex justify-center"
                     :style="{
@@ -49,28 +51,49 @@
                         borderColor: 'purple',
                     }"
                 >
-                    <PkmnDexImg
-                        v-if="h <= genMax && h >= genMin"
-                        :key="h"
-                        :pokeDexNum="{ genNum: 1, numInDex: h }"
-                        :parent_height="picHeight"
-                        :parent_width="picWidth"
-                    />
+                    <div
+                        v-if="h == numInDex + i - 1 && i == 1"
+                        class="border-2 border-red-700"
+                        ><AlignmentCenterDiv>
+                            <PkmnDexImg
+                                v-if="h <= genMax && h >= genMin"
+                                :key="h"
+                                :pokeDexNum="{ genNum: 1, numInDex: h }"
+                                :parent_height="picHeight"
+                                :parent_width="picWidth"
+                            />
+                        </AlignmentCenterDiv>
+                    </div>
+                    <div v-else>
+                        <PkmnDexImg
+                            v-if="h <= genMax && h >= genMin"
+                            :key="h"
+                            :pokeDexNum="{ genNum: 1, numInDex: h }"
+                            :parent_height="picHeight"
+                            :parent_width="picWidth"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
-
+        <!--
         <div class="flex">
-            <PkmnDexImg :pokeDexNum="{ genNum: 1, numInDex: numInDex - 1 }" />
             <PkmnDexImg
+                v-if="numInDex - 1 <= genMax && numInDex >= genMin"
+                :pokeDexNum="{ genNum: 1, numInDex: numInDex - 1 }"
+            />
+            <PkmnDexImg
+                v-if="numInDex <= genMax && numInDex >= genMin"
                 :pokeDexNum="{ genNum: 1, numInDex: numInDex }"
                 :parent_height="picHeight"
                 :parent_width="picWidth"
             />
-            <PkmnDexImg :pokeDexNum="{ genNum: 1, numInDex: numInDex + 1 }" />
-        </div>
+            <PkmnDexImg
+                v-if="numInDex + 1 <= genMax && numInDex >= genMin"
+                :pokeDexNum="{ genNum: 1, numInDex: numInDex + 1 }"
+            />
+        </div> -->
     </div>
-    <span>Number: {{ numInDex }}</span>
 </template>
 
 <style scoped></style>
