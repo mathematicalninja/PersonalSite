e8de2730d502100ab0e72192b3399fa737a56daa
mathematicalninja <github@mathematics.ninja>
2025-05-22 12:55:51 +0800

    Listified tailwind

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -66,10 +66,19 @@ export const buttonVariants = cva(
                     'hover:text-accent-foreground',
                     'dark:hover:bg-accent/50',
                 ],
-                link: ['text-primary', 'underline-offset-4', 'hover:underline'],
+                link: [
+                    'text-primary', //
+                    'underline-offset-4',
+                    'hover:underline',
+                ],
             },
             size: {
-                default: ['h-9', 'px-4', 'py-2', 'has-[>svg]:px-3'],
+                default: [
+                    'h-9', //
+                    'px-4',
+                    'py-2',
+                    'has-[>svg]:px-3',
+                ],
 
                 sm: [
                     'h-8',
@@ -78,7 +87,12 @@ export const buttonVariants = cva(
                     'px-3',
                     'has-[>svg]:px-2.5',
                 ],
-                lg: ["'h-10", 'rounded-md', 'px-6', 'has-[>svg]:px-4'],
+                lg: [
+                    "'h-10", //
+                    'rounded-md',
+                    'px-6',
+                    'has-[>svg]:px-4',
+                ],
 
                 icon: 'size-9',
             },
